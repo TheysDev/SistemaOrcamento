f33896d7fd0b69ab475cdaf5cfb75973f4808542
theus <theys.dev@gmail.com>
2026-07-15 22:17:23 -0300

    Commit Inicial

---
+++ OrcamentoSaaS.Api/Data/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OrcamentoSaaS.Api.Domain.Entities;
+using OrcamentoSaaS.Api.Domain.Interfaces;
+
+namespace OrcamentoSaaS.Api.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options, ITenantProvider tenantProvider) : IdentityDbContext<AppUser>(options)
+{
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Fornecedor> Fornecedores { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Orcamento> Orcamentos { get; set; }
+    public DbSet<ItemOrcamento> ItemOrcamentos { get; set; }
+    public DbSet<Pedido> Pedidos { get; set; }
+    public DbSet<Parcela> Parcelas { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        
+        modelBuilder.Entity<Cliente>().HasQueryFilter("SoftDelete", c => c.IsActive)
+            .HasQueryFilter("TenantFilter", c => c.TenantId == tenantProvider.TenantId);
+        
+        modelBuilder.Entity<Produto>().HasQueryFilter("SoftDelete", c => c.IsActive)
+            .HasQueryFilter("TenantFilter", c => c.TenantId == tenantProvider.TenantId);
+        
+        modelBuilder.Entity<Fornecedor>().HasQueryFilter("SoftDelete", c => c.IsActive)
+            .HasQueryFilter("TenantFilter", c => c.TenantId == tenantProvider.TenantId);
+        
+        modelBuilder.Entity<Orcamento>().HasQueryFilter("SoftDelete", c => c.IsActive)
+            .HasQueryFilter("TenantFilter", c => c.TenantId == tenantProvider.TenantId);
+        
+        modelBuilder.Entity<ItemOrcamento>().HasQueryFilter("SoftDelete", c => c.IsActive)
+            .HasQueryFilter("TenantFilter", c => c.TenantId == tenantProvider.TenantId);
+        
+        modelBuilder.Entity<Pedido>().HasQueryFilter("SoftDelete", c => c.IsActive)
+            .HasQueryFilter("TenantFilter", c => c.TenantId == tenantProvider.TenantId);
+        
+        modelBuilder.Entity<Parcela>().HasQueryFilter("TenantFilter", c => 
+            c.TenantId == tenantProvider.TenantId);
+    }
+}
---
+++ OrcamentoSaaS.Api/Program.cs
@@ -0,0 +1,49 @@
+using OrcamentoSaaS.Api.Features.Clientes.Create;
+using OrcamentoSaaS.Api.Features.Clientes.Shared;
+using OrcamentoSaaS.Api.Features.Fornecedores.Create;
+using OrcamentoSaaS.Api.Features.Fornecedores.Shared;
+using OrcamentoSaaS.Api.Features.Produtos.Create;
+using OrcamentoSaaS.Api.Features.Produtos.Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string" + "'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication();
+
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<ITenantProvider, TenantProvider>();
+
+//DI Create
+builder.AddCliente().AddFornecedor().AddProduto();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.UseHttpsRedirection();
+
+app.UseAuthentication();   
+app.UseAuthorization();  
+
+app.MapIdentityApi<AppUser>();
+
+//EndPoints
+//Create
+app.ProdutoCreateRoute();
+app.CreateClienteRoute();
+app.FornecedorCreateRoute();
+
+app.Run();
+
