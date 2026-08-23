e2c972f87a2beb48ba1c0db1a0519bb5d6a6cfd3
theus <theys.dev@gmail.com>
2026-08-23 16:55:55 -0300

    chore: Adicionando Migrations

---
+++ OrcamentoSaaS.Api/Migrations/20260822184534_AddIndexOrcamentoClienteDetalhes.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrcamentoSaaS.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexOrcamentoClienteDetalhes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Valor",
+                table: "tb_ItemOrcamento",
+                newName: "ValorItem");
+
+            migrationBuilder.RenameColumn(
+                name: "Desconto",
+                table: "tb_ItemOrcamento",
+                newName: "DescontoItem");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orcamento_Tenant_Cliente_Codigo",
+                table: "tb_Orcamento",
+                columns: new[] { "TenantId", "ClienteId", "IsActive", "Codigo" },
+                descending: new[] { false, false, false, true })
+                .Annotation("SqlServer:Include", new[] { "Status", "Validade", "FornecedorId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Orcamento_Tenant_Cliente_Codigo",
+                table: "tb_Orcamento");
+
+            migrationBuilder.RenameColumn(
+                name: "ValorItem",
+                table: "tb_ItemOrcamento",
+                newName: "Valor");
+
+            migrationBuilder.RenameColumn(
+                name: "DescontoItem",
+                table: "tb_ItemOrcamento",
+                newName: "Desconto");
+        }
+    }
+}
